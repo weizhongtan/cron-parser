7bfb281475fcacaf073f410651147a2283285c12
weizhongtan <me@wztan.com>
2021-07-28 23:19:59 +0100

    implement buildTable

--- src/build-table.js
+++ src/build-table.js
@@ -1,3 +1,17 @@
-export const buildTable = (input) => {
-  return input;
+const COL_BUF = 14;
+
+export const buildTable = (fields) => {
+  const specificRowMappings = {
+    dayOfMonth: "day of month",
+    dayOfWeek: "day of week",
+  };
+
+  return ["minute", "hour", "dayOfMonth", "month", "dayOfWeek", "command"]
+    .map((rowName) => {
+      return [
+        (specificRowMappings[rowName] || rowName).padEnd(COL_BUF),
+        fields[rowName],
+      ].join("");
+    })
+    .join("\n");
 };
